cc52f31427487a842e2ecd60caae7fbde98e82d7
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-21 16:53:44 +0300

    updated: ui design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlindetailedrecyclerviewexample"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlindetailedrecyclerviewexample"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlindetailedrecyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlindetailedrecyclerviewexample/MainActivity.kt
@@ -2,12 +2,17 @@ package com.ismailmesutmujde.kotlindetailedrecyclerviewexample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlindetailedrecyclerviewexample.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
 
     }
 }
